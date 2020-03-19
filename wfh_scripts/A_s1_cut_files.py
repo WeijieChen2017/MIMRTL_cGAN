2c75ac32147ec530b3f421928c0f2f95ac401644
WeijieChen2017 <weijiechen1994@gmail.com>
2020-03-19 17:18:36 -0500

    3d_unet_support

---
+++ wfh_scripts/A_s1_cut_files.py
@@ -0,0 +1,19 @@
+import os
+import glob
+import numpy as np
+import nibabel as nib
+
+name_dataset = "3dunet"
+
+for folder_name in ["trainA", "trainB", "testA", "testB"]:
+    path = "../pytorch-CycleGAN-and-pix2pix/datasets/"+name_dataset+"/"+folder_name+"/"
+    if not os.path.exists(path):
+        os.makedirs(path)
+        
+blur_path = "../data/"+name_dataset+"/blur/"
+if not os.path.exists(blur_path):
+    os.makedirs(blur_path)
+    
+pure_path = "../data/"+name_dataset+"/pure/"
+if not os.path.exists(pure_path):
+    os.makedirs(pure_path)
