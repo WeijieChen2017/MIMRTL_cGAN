3a5197e7485da4c5f6986b34b7e11a92e3f36e6a
WeijieChen2017 <weijiechen1994@gmail.com>
2020-03-22 21:11:11 -0500

    3d_unet_support

--- pytorch-CycleGAN-and-pix2pix/data/aligned3d_dataset.py
+++ pytorch-CycleGAN-and-pix2pix/data/aligned3d_dataset.py
@@ -57,12 +57,12 @@ class Aligned3dDataset(BaseDataset):
 
         # load npy files
         AB_path = self.AB_paths[index]
-        AB = np.load(AB_path)
+        AB = np.squeeze(np.load(AB_path))
         print(AB.shape)
         # split AB
-        cube_size = AB.shape[1]
-        data_A = AB[:, :, :, :cube_size]
-        data_B = AB[:, :, :, cube_size:]
+        cube_size = AB.shape[0]
+        data_A = AB[:, :, :cube_size]
+        data_B = AB[:, :, cube_size:]
             
         print(data_A.shape)
         print(data_B.shape)
