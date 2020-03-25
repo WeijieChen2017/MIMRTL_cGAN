66fa591f17628951f238586c9af4709cb5c67de9
WeijieChen2017 <weijiechen1994@gmail.com>
2020-03-25 12:11:34 -0500

    3d_unet_support

--- pytorch-CycleGAN-and-pix2pix/models/pix2pix_model.py
+++ pytorch-CycleGAN-and-pix2pix/models/pix2pix_model.py
@@ -47,7 +47,7 @@ class Pix2PixModel(BaseModel):
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
         self.loss_names = ['G_L1']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
-        self.visual_names = ['real_A', 'fake_B']
+        self.visual_names = ['real_A', 'fake_B', 'real_B']
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
         if self.isTrain:
             self.model_names = ['G']
