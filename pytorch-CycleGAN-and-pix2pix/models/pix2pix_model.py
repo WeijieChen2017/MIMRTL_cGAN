875f3713d7ac3eec1464389d4c0e8fe4d00a9ab0
WeijieChen2017 <weijiechen1994@gmail.com>
2020-03-31 15:16:41 -0500

    3d_unet_support

--- pytorch-CycleGAN-and-pix2pix/models/pix2pix_model.py
+++ pytorch-CycleGAN-and-pix2pix/models/pix2pix_model.py
@@ -75,7 +75,7 @@ class Pix2PixModel(BaseModel):
         """
         AtoB = self.opt.direction == 'AtoB'
         self.real_A = input['A' if AtoB else 'B'].to(self.device)
-        # self.real_B = input['B' if AtoB else 'A'].to(self.device)
+        self.real_B = input['B' if AtoB else 'A'].to(self.device)
         self.image_paths = input['A_paths' if AtoB else 'B_paths']
 
     def forward(self):
